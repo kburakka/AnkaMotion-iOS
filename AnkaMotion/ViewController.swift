651aaa770cc91f2721ee59b3ee5b19dd0646af3e
burak kaya <burak95kaya@hotmail.com>
2020-01-16 09:25:22 +0300

    Initial Commit

---
+++ AnkaMotion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnkaMotion
+//
+//  Created by burak kaya on 16/01/2020.
+//  Copyright © 2020 burak kaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
